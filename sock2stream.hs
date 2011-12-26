79e45bbb4dc29beb084e511a4142ed5133a7a5e9
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-12-25 23:02:51 -0500

    Synchronize STDOUT through server

--- sock2stream.hs
+++ sock2stream.hs
@@ -1,6 +1,6 @@
 import Network
 import System.IO
-import Control.Monad (unless)
+import Control.Monad (forever,when)
 import Control.Concurrent.CHP
 import Data.Binary
 import qualified Data.ByteString.Lazy as LZ
@@ -8,30 +8,38 @@ import qualified Data.ByteString.Lazy as LZ
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 
+-- Connection ID and data chunk
+data Msg = Msg Word32 LZ.ByteString
+
 main :: IO ()
 main = withSocketsDo . runCHP_ $ do
+	(stdoutRecv, stdoutSend) <- newChannelRW
 	sock <- liftIO_CHP $ listenOn (UnixSocket "/tmp/test")
-	listen sock 0
+	stdoutServer stdoutRecv <|*|> listen sock stdoutSend
+
+stdoutServer :: Chanin Msg -> CHP ()
+stdoutServer stdoutRecv = forever $ do
+	(Msg id chunk) <- readChannel stdoutRecv
+	liftIO_CHP $ do
+		let length = fromIntegral $ LZ.length chunk
+		LZ.putStr $ encode id
+		LZ.putStr $ encode (length :: Word32)
+		LZ.putStr chunk
 
 -- TODO: Replace incrementing IDs with ID server
-listen :: Socket -> Word32 -> CHP ()
-listen sock next = do
-	handle <- doAccept
-	listen sock (next + 1) <|*|> handle2stdout next handle
+listen :: Socket -> Shared Chanout Msg -> CHP ()
+listen sock stdoutSend = listen' 0
 	where
+	listen' next = do
+		handle <- doAccept
+		listen' (next + 1) <|*|> handle2stdout next handle stdoutSend
 	doAccept = liftIO_CHP $ do
 		(handle,_,_) <- accept sock
 		hSetBinaryMode handle True
 		return handle
 
-handle2stdout :: Word32 -> Handle -> CHP ()
-handle2stdout id handle = liftIO_CHP doChunk
-	where
-	doChunk = do
-		chunk <- LZ.hGetNonBlocking handle 2048
-		let length = fromIntegral (LZ.length chunk)
-		unless (length < 1) $ do
-			LZ.putStr $ encode id
-			LZ.putStr $ encode (length :: Word32)
-			LZ.putStr chunk
-		doChunk
+handle2stdout :: Word32 -> Handle -> Shared Chanout Msg -> CHP ()
+handle2stdout id handle stdoutSend = forever $ do
+	chunk <- liftIO_CHP $ LZ.hGetNonBlocking handle 2048
+	when (LZ.length chunk > 0) $
+		claim stdoutSend (`writeChannel` Msg id chunk)
